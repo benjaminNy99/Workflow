d2d26974c9edcce71721734e184e8096ed5861b1
benjaminNy99 <benja.ignacio2001@gmail.com>
2026-02-23 22:11:07 -0300

    add dtos para state y priority entitys

---
+++ Workflow.Application/DTOs/PriorityDtos/PriorityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Workflow.Application.DTOs.PriorityDtos
+{
+    public class PriorityDto
+    {
+        public int Code { get; set; }
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ Workflow.Application/DTOs/StateDtos/StateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Workflow.Application.DTOs.StateDtos
+{
+    public class StateDto
+    {
+        public int Code { get; set; }
+        public string Description { get; set; } = null!;
+    }
+}
