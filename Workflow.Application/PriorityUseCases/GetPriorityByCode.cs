ee39c1167dd522ccced0a80743064a5180a0d9cb
benjaminNy99 <benja.ignacio2001@gmail.com>
2026-02-23 22:07:35 -0300

    add use cases para prioriry y state entitys

---
+++ Workflow.Application/PriorityUseCases/GetPriorityByCode.cs
@@ -0,0 +1,20 @@
+﻿using Workflow.Application.Services;
+using Workflow.Domain;
+
+namespace Workflow.Application.PriorityUseCase
+{
+    public sealed class GetPriorityByCode
+    {
+        private ICodeRepository<Priority> _repository;
+
+        public GetPriorityByCode(ICodeRepository<Priority> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Priority?> ExecuteAsync(int code)
+        {
+            return await _repository.GetAsync(code);
+        }
+    }
+}
---
+++ Workflow.Application/StateUseCades/GetStateByCode.cs
@@ -0,0 +1,20 @@
+﻿using Workflow.Application.Services;
+using Workflow.Domain;
+
+namespace Workflow.Application.StateUseCades
+{
+    public sealed class GetStateByCode
+    {
+        private readonly ICodeRepository<State> _repository;
+
+        public GetStateByCode(ICodeRepository<State> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<State?> Execute(int code)
+        {
+            return await _repository.GetAsync(code);
+        }
+    }
+}
