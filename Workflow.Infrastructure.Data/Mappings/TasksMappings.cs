cb6d283a1e33135ffd72ad4e201159538eb51b8a
benjaminNy99 <benja.ignacio2001@gmail.com>
2026-02-26 00:55:45 -0300

    add repository y mapping para tasks

---
+++ Workflow.Infrastructure.Data/Mappings/TasksMappings.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using Workflow.Domain;
+using Workflow.Infrastructure.Data.Models;
+
+namespace Workflow.Infrastructure.Data.Mappings
+{
+    internal static class TasksMappings
+    {
+        internal static Expression<Func<Tasks, TasksEntity>> ToDomain =>
+            t => new TasksEntity(
+                id: t.Id,
+                name: t.Name,
+                description: t.Description,
+                stateCode: t.StateCode,
+                priorityCode: t.PriorityCode);
+
+        internal static Tasks ToModel(this TasksEntity t)
+            => new Tasks
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description,
+                StateCode = t.StateCode,
+                PriorityCode = t.PriorityCode,
+            };
+    }
+}
---
+++ Workflow.Infrastructure.Data/Repositories/TasksRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Workflow.Application.Services;
+using Workflow.Application.TasksUseCases;
+using Workflow.Domain;
+using Workflow.Infrastructure.Data.Mappings;
+using Workflow.Infrastructure.Data.Models;
+
+namespace Workflow.Infrastructure.Data.Repositories
+{
+    public sealed class TasksRepository : IRepository<TasksEntity, Guid>, ITasks
+    {
+        private readonly Context _context;
+
+        public TasksRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TasksEntity entity)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            await _context.AddAsync(entity.ToModel());
+        }
+
+        public Task DeleteAsync(TasksEntity entity)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            _context.Remove(entity.ToModel());
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<TasksEntity>> GetAllAsync(StateEntity state)
+        {
+            return await _context.Tasks
+                .Where(t => t.StateCode == state.Code)
+                .Select(TasksMappings.ToDomain)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TasksEntity>> GetAllAsync(PriorityEntity priority)
+        {
+            return await _context.Tasks
+                .Where(t => t.PriorityCode == priority.Code)
+                .Select(TasksMappings.ToDomain)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TasksEntity>> GetAllAsync(StateEntity state, PriorityEntity priority)
+        {
+            return await _context.Tasks
+                .Where(t => t.StateCode == state.Code && t.PriorityCode == priority.Code)
+                .Select(TasksMappings.ToDomain)
+                .ToListAsync();
+        }
+
+        public async Task<TasksEntity?> GetAsync(Guid id)
+        {
+            return await _context.Tasks
+                .Where (t => t.Id == id)
+                .Select(TasksMappings.ToDomain)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
