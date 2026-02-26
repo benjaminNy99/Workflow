4ca1cba8c61db0cd8d29c514dd26cab465c27ec0
benjaminNy99 <benja.ignacio2001@gmail.com>
2026-02-26 19:45:01 -0300

    add repository para state entity

---
+++ Workflow.Infrastructure.Data/Mappings/StateMappings.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Workflow.Domain;
+using Workflow.Infrastructure.Data.Models;
+
+namespace Workflow.Infrastructure.Data.Mappings
+{
+    internal static class StateMappings
+    {
+        internal static Expression<Func<State, StateEntity>> ToDomain =>
+            s => new StateEntity(
+                code: s.Code,
+                description: s.Description);
+
+        internal static State ToModel(this StateEntity s)
+            => new State
+            {
+                Code = s.Code,
+                Description = s.Description,
+            };
+    }
+}
---
+++ Workflow.Infrastructure.Data/Repositories/StateRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Workflow.Application.Services;
+using Workflow.Domain;
+using Workflow.Infrastructure.Data.Mappings;
+using Workflow.Infrastructure.Data.Models;
+
+namespace Workflow.Infrastructure.Data.Repositories
+{
+    public sealed class StateRepository : ICodeRepository<StateEntity>
+    {
+        private readonly Context _context;
+
+        public StateRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<StateEntity?> GetAsync(int code)
+        {
+            return await _context.State
+                .Where(s => s.Code == code)
+                .Select(StateMappings.ToDomain)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
